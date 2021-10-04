687e135a809d9b488269ed85adfbb9c2187f2275
Jonas Kello <jonas.kello@gmail.com>
2021-10-04 20:59:20 +0200

    Test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/src/**/?(*.)+(spec|test).[jt]s?(x)"],
-  collectCoverage: true,
+  collectCoverage: false, // we use --coverage in the script instead
   collectCoverageFrom: ["src/**/*.ts"],
   coverageReporters: ["text-summary", "lcov"],
 };
