783635e685f60ad359519d0ede4aa997b2196972
Jonas Kello <jonas.kello@gmail.com>
2021-10-05 23:34:02 +0200

    Tsconfig

--- src/__tests__/ts-resolve-cases.test.ts
+++ src/__tests__/ts-resolve-cases.test.ts
@@ -15,26 +15,36 @@ onlySkip(tests).forEach((item) => {
     const { entryTsConfig, unsresolvedEntryTsFilePath, resolvedFileUrl, cwd, mfs } = item;
     const fileSystem = createFilesystem(mfs, cwd);
     // parentURL is undefined for the entry file
-    type StackItem = { parentURL: string | undefined; unresolved: string; expectedUrl: string };
+    type StackItem = {
+      parentURL: string | undefined;
+      unresolved: string;
+      expectedUrl: string;
+    };
     const importsStack: Array<StackItem> = [];
-    importsStack.push({ parentURL: undefined, unresolved: unsresolvedEntryTsFilePath, expectedUrl: resolvedFileUrl });
+    importsStack.push({
+      parentURL: undefined,
+      unresolved: unsresolvedEntryTsFilePath,
+      expectedUrl: resolvedFileUrl,
+    });
 
     while (importsStack.length > 0) {
       const { parentURL, unresolved, expectedUrl } = importsStack.pop()!;
       const resolved = tsResolve(unresolved, { conditions: [], parentURL }, entryTsConfig, fileSystem);
+      if (resolved === undefined) {
+        fail(`Specifier ${unresolved} resolved to undefined`);
+      }
+      // Get the mock file for the resolved file
+      const mfsPath = fileURLToPath(resolved.fileUrl);
+      const mfsFile = mfs[mfsPath];
+      if (mfsFile.type !== "TsFile") {
+        fail(`Resolved typescript file ${mfsPath} not found in mock file system.`);
+      }
       // eslint-disable-next-line @typescript-eslint/no-loop-func
       test(`Resolve ${unresolved} (${(parentURL && fileURLToPath(parentURL)) ?? "undefined"})`, () => {
         // Assert resolved url
-        expect(resolved?.fileUrl).toBe(expectedUrl);
+        expect(resolved.fileUrl).toBe(expectedUrl);
+        // expect(resolved.tsConfigUrl).toBe(mfsFile.tsconfig);
       });
-      if (resolved === undefined) {
-        throw new Error("Resolve failed");
-      }
-      // Get the mock file for the resolved file
-      const mfsFile = mfs[fileURLToPath(resolved.fileUrl)];
-      if (mfsFile.type !== "TsFile") {
-        throw new Error("Resolved typescript file not found in mock file system.");
-      }
       importsStack.push(
         ...mfsFile.imports.map((imp) => ({
           parentURL: resolved.fileUrl,
